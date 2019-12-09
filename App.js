9f412c00a1edc015378e116c3e90b55434fd578c
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2019-12-09 16:49:01 -0300

    Ajustes Layout e Clique nos botões

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native'
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Alert } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
@@ -8,28 +8,52 @@ export default class App extends React.Component{
     displayValue: '0'
   }
 
+  addDigit = n =>{
+    this.setState({ displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0' })
+  }
+
+  setOperation = operation => {
+
+  }
+
+  showCreditos = () =>{
+    Alert.alert('Ajuda', 'Selecione a opção desejada.', [
+      {text: 'Relatar bug', onPress: this.relatarBug},
+      {text: 'Fechar', onPress: () => console.log('Créditos fechado')}
+    ])
+  }
+
+  relatarBug = () =>{
+    console.log('Clicou em Relatar bug.');
+  }
+
   render(){
     return(
       <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
+          <Button label='AC' double onClick={this.clearMemory} />
+          <Button label='?' creditos onClick={this.showCreditos} />
+          <Button label='/' operation onClick={() => this.setOperation('/')} />
+          <Button label='7' onClick={() => this.addDigit(7)} />
+          <Button label='8' onClick={() => this.addDigit(8)} />
+          <Button label='9' onClick={() => this.addDigit(9)} />
+          <Button label='*' operation onClick={() => this.setOperation('*')} />
+          <Button label='4' onClick={() => this.addDigit(4)} />
+          <Button label='5' onClick={() => this.addDigit(5)} />
+          <Button label='6' onClick={() => this.addDigit(6)} />
+          <Button label='-' operation onClick={() => this.setOperation('-')} />
+          <Button label='1' onClick={() => this.addDigit(1)} />
+          <Button label='2' onClick={() => this.addDigit(2)} />
+          <Button label='3' onClick={() => this.addDigit(3)} />
+          <Button label='+' operation onClick={() => this.setOperation('+')} />
+          <Button label='0' double onClick={() => this.addDigit(0)} />
+          <Button label='.' onClick={() => this.addDigit('.')} />
+          <Button label='=' operation onClick={() => this.setOperation('=')} />
         </View>
       </View>
     );
